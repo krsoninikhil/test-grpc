c901943836036255ace5684cbe07e6719fa2cc91
Nikhil Kr. Soni <nikhil@uniacco.com>
2022-06-14 15:00:26 +0530

    Add proto files, generated code and client server main function

---
+++ client.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"fmt"
+	"time"
+	"context"
+	"io"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/krsoninikhil/test-grpc/protos"
+)
+
+func oneOnOneResult(client pb.InterviewClient, user *pb.User) {
+	log.Printf("Get result for user")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if result, err := client.OneOnOne(ctx, user); err != nil {
+		fmt.Printf("%v", err)
+	} else {
+		fmt.Printf("Result: %t, %s\n", result.Selected, result.Remark)
+	}
+}
+
+func onlineScreeningResult(client pb.InterviewClient, batch *pb.Batch) {
+	log.Printf("Get result for the batch")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := client.OnlineScreening(ctx, batch)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+	for {
+		result, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error reading stream: %v", err)
+		}
+		fmt.Printf("Streamed Result: %t, %s\n", result.Selected, result.Remark)
+
+	}
+}
+
+func finalRoundResult(client pb.InterviewClient, batch *pb.Batch) {
+	log.Printf("Get result report for streamed user")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := client.FinalRound(ctx)
+	if err != nil {
+		log.Fatalf("Cannot get stream: %v", err)
+	}
+
+	for _, user := range batch.Users {
+		if err := stream.Send(user); err != nil {
+			log.Fatalf("Error in sending: %v", err)
+		}
+	}
+	if resultReport, err := stream.CloseAndRecv(); err != nil {
+		log.Fatalf("Error in closing stream: %v", err)
+	} else {
+		for _, result := range resultReport.Results {
+			fmt.Printf("Result %t, %s\n", result.Selected, result.Remark)
+		}
+	}
+}
+
+
+func campusDriveResult(client pb.InterviewClient, batch *pb.Batch) {
+	log.Printf("Get streamed result for stream users")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := client.CampusDrive(ctx)
+	if err != nil {
+		log.Fatalf("Cannot get stream: %v", err)
+	}
+
+	for _, user := range batch.Users {
+		if err := stream.Send(user); err != nil {
+			fmt.Printf("Cannot send to stream: %v", err)
+		}
+	}
+	for {
+		result, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Cannot read from stream: %v", err)
+		}
+		log.Printf("Result for duplex: %t, %s\n", result.Selected, result.Remark)
+	}
+	stream.CloseSend()
+}
+
+
+func main() {
+	flag.Parse()
+	serverAddr := flag.String("addr", "localhost:8080", "Server address in format host:port")
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("Fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewInterviewClient(conn)
+
+	batch := pb.Batch{
+		Users: []*pb.User{
+			&pb.User{Name: "Test User 1", YearOfExp: 5},
+			&pb.User{Name: "Test User 2", YearOfExp: 5},
+			&pb.User{Name: "Test User 3", YearOfExp: 5},
+		},
+	}
+
+	oneOnOneResult(client, batch.Users[0])
+	onlineScreeningResult(client, &batch)
+	finalRoundResult(client, &batch)
+	campusDriveResult(client, &batch)
+}
---
+++ server.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+import "flag"
+import "context"
+import "net"
+import "log"
+import "io"
+
+import "google.golang.org/grpc"
+
+import pb "github.com/krsoninikhil/test-grpc/protos"
+
+
+type interviewServer struct {
+	pb.UnimplementedInterviewServer
+}
+
+func (is interviewServer) OneOnOne(ctx context.Context, user *pb.User) (*pb.Result, error) {
+	print("One on one")
+	return &pb.Result{Selected: true, Remark: fmt.Sprintf("Above average, %s", user.Name)}, nil
+}
+
+func (is interviewServer) OnlineScreening(batch *pb.Batch, stream pb.Interview_OnlineScreeningServer) error {
+	for _, user := range batch.Users {
+		result := &pb.Result{Selected: true, Remark: fmt.Sprintf("Good work %s", user.Name)}
+		if err := stream.Send(result); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (is interviewServer) FinalRound(stream pb.Interview_FinalRoundServer) error {
+	var users []*pb.User
+	for {
+		user, err := stream.Recv()
+		if err == io.EOF {
+			resultReport := &pb.ResultReport{Results: []*pb.Result{}}
+			for _, user := range users {
+				resultReport.Results = append(
+					resultReport.Results,
+					&pb.Result{Selected: true, Remark: fmt.Sprintf("Good work streamed: %s", user.Name)},
+				)
+			}
+			err := stream.SendAndClose(resultReport)
+			return err
+		}
+		if err != nil {
+			log.Fatalf("Failed to receive: %v", err)
+		}
+		users = append(users, user)
+	}
+
+}
+
+func (is interviewServer) CampusDrive(stream pb.Interview_CampusDriveServer) error {
+	for {
+		user, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Printf("Failed to receive: %v", err)
+			return err
+		}
+		err = stream.Send(&pb.Result{Selected: true, Remark: fmt.Sprintf("Well done %s", user.Name)})
+		if err != nil {
+			log.Printf("Failed to receive: %v", err)
+			return err
+		}
+	}
+}
+
+func main() {
+	port := flag.Int("port", 8080, "The server port")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to start server %v", err)
+	}
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterInterviewServer(grpcServer, interviewServer{})
+	grpcServer.Serve(lis)
+}
